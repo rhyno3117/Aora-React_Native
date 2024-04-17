eb144316c0883905900c9c3dfae3a8d76ee14981
ryan <rhynogolfer42@gmail.com>
2024-04-16 19:52:27 -0700

    Created layout and index and layout displays index with Slot

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import { Slot } from 'expo-router' //Slot renders current child route
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const RootLayout = () => {
+    return <Slot />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default RootLayout
+
