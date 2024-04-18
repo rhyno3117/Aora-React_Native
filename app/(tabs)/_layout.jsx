b510335cdc71427f9d04cb12af8c6b6f8c90c2ca
ryan <rhynogolfer42@gmail.com>
2024-04-17 21:36:04 -0700

    added styles for navbar at the bottom with the color and sizes

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -11,7 +11,10 @@ const TabIcon = ({ icon, color, name, focused }) => {
         tintColor={color}
         className="w-6 h-6"
       />
-      <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}>
+      <Text 
+      className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}
+      style={{ color: color }}
+      >
         {name}
       </Text>
     </View>
@@ -23,7 +26,16 @@ const TabsLayout = () => {
     <>
       <Tabs
         screenOptions={{
-          tabBarShowLabel: false
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: '#FFA001',
+          tabBarInactiveTintColor: '#CDCDE0',
+          tabBarStyle: {
+            backgroundColor: '#161622',
+            borderTopWidth: 1,
+            borderTopColor: '#232533',
+            height: 84,
+          }
+
         }}
       >
         <Tabs.Screen
