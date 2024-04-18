4c3ca5ff23dfd9084a03810117cafc61e190150e
ryan <rhynogolfer42@gmail.com>
2024-04-17 22:06:28 -0700

    added images logo and card

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { View, Text, Image, ScrollView } from "react-native";
 import { Link } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from '../constants';
 
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image 
+          source={images.cards}
+          className="max-w-[380px] w-full h-[300px]"
+          resizeMode="contain"
+          />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
