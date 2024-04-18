6e024e2f8c069267c3426c40181272c3c0c4928d
ryan <rhynogolfer42@gmail.com>
2024-04-17 22:39:37 -0700

    added styling to CustomButton.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -35,7 +35,11 @@ export default function App() {
           </View>
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
           Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
-          <CustomButton />
+          <CustomButton 
+          title="Continue with Email"
+          handlePress={() => {}}
+          containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -1,12 +1,17 @@
-import {Text, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
 import React from 'react'
 
-const CustomButton = () => {
-  return (
-    <TouchableOpacity  className={`bg-secondary rounded-xl min-h-[62px] flex flex-row justify-center items-center`}>
-      <Text>CustomButton</Text>
-    </TouchableOpacity>
-  )
+const CustomButton = ({ title, handlePress, containerStyles, textStyles, isLoading }) => {
+    return (
+        <TouchableOpacity
+            onPress={handlePress}
+            activeOpacity={0.7}
+            className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}
+            disabled={isLoading}
+        >
+            <Text className={`text-primary font-psemibold text-lg ${textStyles}`}>{title}</Text>
+        </TouchableOpacity>
+    )
 }
 
 export default CustomButton
