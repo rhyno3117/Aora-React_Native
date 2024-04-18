79cb25a8ce0bee87db52890af4592afb53c83a79
ryan <rhynogolfer42@gmail.com>
2024-04-17 21:51:15 -0700

    code before removing first screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function App() {
   return (
