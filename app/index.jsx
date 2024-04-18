090532392b3f2f8eb871ead7ef197c89255792ac
ryan <rhynogolfer42@gmail.com>
2024-04-17 20:55:20 -0700

    folder structure created

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Aora!</Text>
+      <Text className="text-3xl font-pblack">Aora!</Text>
       <StatusBar style="auto" />
       <TouchableOpacity onPress={() => console.log('Navigate to Profile')} className="mt-4">
         <Text className="text-blue-500">Go to Profile</Text>
