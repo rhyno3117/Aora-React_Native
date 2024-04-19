09aa5167638e1f0ad1606414cbae85d10b21c6e6
ryan <rhynogolfer42@gmail.com>
2024-04-19 10:56:22 -0700

    added useState for a form for email and password in sign-in.jsx

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,17 +1,34 @@
 import { View, Text, ScrollView, Image } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants';
 import FormField from '../../components/FormField';
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
   return (
     <SafeAreaView className='bg-primary h-full'>
       <ScrollView>
         <View className='w-full justify-center h-full px-4 my-6'>
-          <Image source={images.logo} resizeMode='contain'className='w-[115px] h-[35px]'/>
+          <Image source={images.logo} resizeMode='contain' className='w-[115px] h-[35px]' />
           <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
-          <FormField />
+          <FormField
+            title='Email'
+            value={form.email}
+            handleChange={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title='Password'
+            value={form.password}
+            handleChange={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
