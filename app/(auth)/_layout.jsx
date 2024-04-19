f5a35ca4213a5a828c504d62e86e1c294917a47a
ryan <rhynogolfer42@gmail.com>
2024-04-19 10:48:09 -0700

    added layout of sign-in.jsx file in (auth) folder

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native'
 import { Stack } from 'expo-router'
-import React from 'react'
+import { StatusBar } from 'expo-status-bar'
 
 const AuthLayout = () => {
   return (
@@ -19,6 +19,7 @@ const AuthLayout = () => {
           }}
         />
       </Stack>
+      <StatusBar backgroundColor="#161622" style="light" />
     </>
   )
 }
--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from '../../constants';
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView>
+        <View className='w-full justify-center h-full px-4 my-6'>
+          <Image source={images.logo} resizeMode='contain'className='w-[115px] h-[35px]'/>
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
