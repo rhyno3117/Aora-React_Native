4d4363b8db283b324bc0eaf08b6a6da5b87467d1
ryan <rhynogolfer42@gmail.com>
2024-04-19 10:52:40 -0700

    created in components folder FormField.jsx and imported it into sign-in.jsx

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -2,6 +2,7 @@ import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants';
+import FormField from '../../components/FormField';
 
 const SignIn = () => {
   return (
@@ -10,6 +11,7 @@ const SignIn = () => {
         <View className='w-full justify-center h-full px-4 my-6'>
           <Image source={images.logo} resizeMode='contain'className='w-[115px] h-[35px]'/>
           <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Log in to Aora</Text>
+          <FormField />
         </View>
       </ScrollView>
     </SafeAreaView>
