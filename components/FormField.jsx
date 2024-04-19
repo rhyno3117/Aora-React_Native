7a058dfa4856a1f10f327c0e75797832d48663ad
ryan <rhynogolfer42@gmail.com>
2024-04-19 10:59:52 -0700

    back in FormField.jsx in components, added props and also added the View of the textbox

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const FormField = () => {
+const FormField = ({ title, value, placeholder, handleChangeText, otherStyles, ...props }) => {
   return (
-    <View className="space-y-2">
-      <Text className="text-base text-gray-100 font-pmedium">FormField</Text>
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+      <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center">
+
+      </View>
     </View>
   )
 }
