a5f5bb3cfe6bb066c0e6f0e29bd0d75925ead472
ryan <rhynogolfer42@gmail.com>
2024-04-26 19:27:05 -0700

    function for newUser and SignIn have been created

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -47,7 +47,7 @@ const SignUp = () => {
             otherStyles="mt-7"
           />
           <CustomButton
-            title='Sign In'
+            title='Sign Up'
             handlePress={submit}
             containerStyles="mt-7"
             isLoading={isSubmitting}
