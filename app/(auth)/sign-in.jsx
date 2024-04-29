dfb3103f8db973fb5c32e972713fa8a746aabf61
ryan <rhynogolfer42@gmail.com>
2024-04-28 18:58:55 -0700

    added logic for sing-in

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,10 +1,11 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
 import { images } from '../../constants';
 import FormField from '../../components/FormField';
 import CustomButton from '../../components/CustomButton'
+import { signIn }  from '../../lib/app.write'
 
 const SignIn = () => {
   const [form, setForm] = useState({
@@ -14,7 +15,20 @@ const SignIn = () => {
 
   const [isSubmitting, setIsSubmitting] = useState(false)
 
-  const submit = () => {
+  const submit = async () => {
+    if(!form.email || !form.password) {
+      Alert.alert('Error', 'Please fill in all the fields')
+    }
+      setIsSubmitting(true);
+      try {
+        await signIn(form.email, form.password)
+        //set it to global state...
+        router.replace('/home')
+      } catch (error) {
+        Alert.alert('Error', error.message)
+      } finally {
+        setIsSubmitting(false)
+      }
   }
 
   return (
