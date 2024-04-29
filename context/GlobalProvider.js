e4066d8b8f8b230169b62562f8befcc1e784a8de
ryan <rhynogolfer42@gmail.com>
2024-04-28 19:04:58 -0700

    added useStates to GlobalProvider.js file

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -1,1 +1,20 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
+
+const GlobalContext = createContext();
+export const useGlobalContext = () => useContext(GlobalContext);
+
+const GlobalProvider = ({ children }) => {
+    const [isLogged, setIsLogged] = useState(false);
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    return (
+        <GlobalContext.Provider
+            value={{
+
+            }}
+        >
+            {children}
+        </GlobalContext.Provider>
+    )
+}
