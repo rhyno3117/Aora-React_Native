f202572dfa5291fc8f224d33eae318450194534d
ryan <rhynogolfer42@gmail.com>
2024-04-29 22:32:10 -0700

    added globalcontext to index.js

--- app/index.jsx
+++ app/index.jsx
@@ -6,9 +6,13 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from '../constants';
 import { CustomButton } from "../components";
 
+import { useGlobalContext } from '../context/GlobalProvider';
+
 
 
 export default function App() {
+const {isLoading, isLoggedIn} = useGlobalContext();
+
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
