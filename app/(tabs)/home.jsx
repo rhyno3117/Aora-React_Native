1e1e3e180da7854f489fcbe3d5f877466ceec9d3
ryan <rhynogolfer42@gmail.com>
2024-05-02 11:57:42 -0700

    creating home.jsx page tab

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <FlatList 
+      
+      />
+    </SafeAreaView>
   )
 }
 
