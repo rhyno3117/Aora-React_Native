5ce4e403ec233a48e3e518b71911fc391ed54029
ryan <rhynogolfer42@gmail.com>
2024-05-02 12:22:50 -0700

    1 is now appearing on home screen

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Home = () => {
   return (
     <SafeAreaView>
-      <FlatList 
-      
+      <FlatList
+        data={[{ id: 1 }]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <Text className='text-3xl'>{item.id}</Text>
+        )}
       />
     </SafeAreaView>
   )
