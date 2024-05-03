76239b5d3f127c508a938961b695b51a1c3ba7d1
ryan <rhynogolfer42@gmail.com>
2024-05-02 18:05:05 -0700

    added and import image in home

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,16 +1,37 @@
 import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from "../../constants";
+
 
 const Home = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className='bg-primary'>
       <FlatList
         data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
         keyExtractor={(item) => item.$id}
         renderItem={({ item }) => (
-          <Text className='text-3xl'>{item.id}</Text>
+          <Text className='text-3xl text-white'>{item.id}</Text>
         )}
+        ListHeaderComponent={() => {
+          <View className='my-6 px-4 space-y-6'>
+            <View className='justify-between items=start flex-row mb-6'>
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  JSMastery
+                </Text>
+              </View>
+              <View className='mt-1.5'>
+                <Image
+                
+                />
+              </View>
+            </View>
+          </View>
+        }}
       />
     </SafeAreaView>
   )
