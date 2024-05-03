89b11fc2fe0d27b35420ac7920f0ffc2999018ad
ryan <rhynogolfer42@gmail.com>
2024-05-02 19:29:28 -0700

    modifiying SearchInput

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -9,28 +9,17 @@ import { icons } from '../constants'
 const SearchInput = ({ title, value, placeholder, handleChangeText, otherStyles, ...props }) => {
     const [showPassword, setShowPassword] = useState(false)
     return (
-        <View className={`space-y-2 ${otherStyles}`}>
-            <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
-            <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row">
-                <TextInput
-                    className='flex-1 text-white font-psemibold text-base'
-                    value={value}
-                    placeholder={placeholder}
-                    placeholderTextColor="#7b7b8b"
-                    onChangeText={handleChangeText}
-                    secureTextEntry={title === 'Password' && !showPassword}
-                />
+        <View className="border-2 border-black-200 w-full h-16 px-4 bg-black-100 rounded-2xl focus:border-secondary items-center flex-row space-x-4">
+            <TextInput
+                className="text-base mt-0.5 text-white flex-1 font-pregular"
+                value={value}
+                placeholder={placeholder}
+                placeholderTextColor="#7b7b8b"
+                onChangeText={handleChangeText}
+                secureTextEntry={title === 'Password' && !showPassword}
+            />
 
-                {title === "Password" && (
-                    <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-                        <Image
-                            source={!showPassword ? icons.eye : icons.eyeHide}
-                            className="w-6 h-6"
-                            resizeMode="contain"
-                        />
-                    </TouchableOpacity>
-                )}
-            </View>
+        
         </View>
     )
 }
